aa061cea67b03701442dc0b2e41344967065b837
bayareaIA03 <immadarshad03@gmail.com>
2023-05-16 14:45:16 -0700

    Delete upsAPI.js

--- upsAPI.js
+++
@@ -1,8 +0,0 @@
-const upsApi = require("ups-api");
-
-// instance the API client with defaults
-const api = new upsApi.API({
-    username: "myupsaccount",
-    password: "myupsaccountpassword",
-    license: "3F9955C3F789C255"
-});
